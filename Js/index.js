b73df9f3689add5807d8302a57de31a6727a50e8
Nabilhossaim01859 <bdroomcard1@gamil.com>
2024-09-25 07:30:38 +0600

    some of the js functionlities is done such as hidden remove

--- Js/index.js
+++ Js/index.js
@@ -1,7 +1,6 @@
-// main balance amount
+
+
 let balance = 100000;
-
-
 const history = [];
 
 
@@ -10,4 +9,22 @@ const donateButtons = document.querySelectorAll('.donate-now-btn');
 const donationButtons = document.getElementById('donate-btn');
 const historyButtons = document.getElementById('history-btn');
 const mainSection = document.getElementById('donate-cards');
-const modal = document.getElementById('my_modal_2');
+
+
+const historySection = document.createElement('div');
+historySection.classList.add('history-section', 'hidden');
+mainSection.appendChild(historySection);
+
+
+donationButtons.addEventListener('click', () => {
+    mainSection.classList.remove('hidden');
+    historySection.classList.add('hidden');
+});
+
+historyButtons.addEventListener('click', () => {
+    mainSection.classList.add('hidden');
+    historySection.classList.remove('hidden');
+    displayHistory();
+});
+
+
