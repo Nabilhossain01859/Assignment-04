863f9d6a6407230b130e4443d8945f57e0946d1a
Nabilhossaim01859 <bdroomcard1@gamil.com>
2024-09-25 06:44:15 +0600

    working on the js part after debuging how to do that

--- Js/index.js
+++ Js/index.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+// main balance amount
+let balance = 100000;
+
+
+const history = [];
+
+
+const balanceElement = document.getElementById('balance');
+const donateButtons = document.querySelectorAll('.donate-now-btn');
+const donationButtons = document.getElementById('donate-btn');
+const historyButtons = document.getElementById('history-btn');
+const mainSection = document.getElementById('donate-cards');
+const modal = document.getElementById('my_modal_2');
